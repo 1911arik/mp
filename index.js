ffa6c1ced8257a698f77e0174e52f5609c7b890e
Arik <65552670+1911arik@users.noreply.github.com>
2020-10-09 18:21:26 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,78 @@
+{
+
+    alert("Welcome to Test_Calculator!");
+}
+
+
+{
+
+    alert("****Addition!****");
+}
+
+    
+var num1=prompt("Enter First Number:");
+var num2=prompt("Enter Second Number:");
+
+num1=parseInt(num1);
+num2=parseInt(num2);
+
+var sum;
+
+sum=num1+num2;
+document.write("<b>Addition Result=</b> "+ sum +"<br></br>");
+
+
+
+{
+
+    alert("****Subtraction!****");
+}
+var num1=prompt("Enter First Number:");
+var num2=prompt("Enter Second Number:");
+
+num1=parseInt(num1);
+num2=parseInt(num2);
+
+var sum;
+
+sum=num1-num2;
+document.write("<b>Subtraction Result=</b> "+ sum +"<br></br>");
+
+
+
+{
+
+    alert("****Multiplication!****");
+}
+
+var num1=prompt("Enter First Number:");
+var num2=prompt("Enter Second Number:");
+
+num1=parseInt(num1);
+num2=parseInt(num2);
+
+var sum;
+
+sum=num1*num2;
+document.write("<b>Multiplication Result=</b> "+ sum +"<br></br>");
+
+
+
+
+
+
+{
+
+    alert("****Division!****");
+}
+
+var num1=prompt("Enter a large Number:");
+var num2=prompt("Enter a small Number:");
+
+num1=parseInt(num1);
+num2=parseInt(num2);
+
+var sum;
+
+sum=num1/num2;
+document.write("<b>Division Result= </b>"+ sum +"<br></br>");
